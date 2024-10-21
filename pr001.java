24e4a8323f6cb305977c81f633cece8f8179fcea
kapitanov <kapitanov2002611@gmail.com>
2024-10-21 15:41:35 +0700

    работа с типами данными

--- pr001.java
+++ pr001.java
@@ -1,5 +1,37 @@
 class pr001 {
 	public static void main(String args[]) {
 		System.out.println("pervaua java-programma");
+		//преобразование типов
+
+		// цулые типы данных
+		byte b;// 8 бит
+		b = -128;
+		b = 127;
+		b = (byre)130;
+		short sh; // 16бит
+	        sh = (short)65536;
+		int intg; // 32
+		long l // 64
+		intg = 2000000000
+		intg = intg*intg;
+		system.out.printl(intg);
+		l = l*l*l;
+		system.out.println(l);
+
+		//вещественные типы
+		float f; //32
+		double dd; //64
+		// символьный тип
+		char ch; // 16 бит
+	        ch = 'A';
+		ch++; ++ch;
+		for(int i=0; i<68; i++)
+			System.out.println(ch++ + " ");
+		System.out.println();
+		// логический тип;
+		boolean bool;
+	        bool = true;
+		bool = false;
+       	       
 	}
 }
