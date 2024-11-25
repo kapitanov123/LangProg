7eb83b8a1ef52325b2d1a0670d7b6f3d7a367643
kapitanov <kapitanov2002611@gmail.com>
2024-11-25 15:52:02 +0700

    добавили перегружаемый конструктор для создания очереди на базе другой очереди массива

--- Queue.java
+++ Queue.java
@@ -1,9 +1,28 @@
 class Queue {
 	private char[] q;
 	private int putloc, getloc; //
+	//создается пустая очередб заданного размера
 	Queue(int size) {
 		q = new char[size]; //
 		putloc=getloc=0;
+	}
+	//создается очередь на основе другого объекта
+	
+	Queue(Queue ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+		//копирование элемнтов массива
+		for(int i = getloc; i<putloc; i++)
+			q[i] = ob.q[i];
+	}
+	//создание очереди с начальными значениямина основе массива
+	Queue(char[] a) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+		for(int i =0; i<a.length; i++)
+			put(a[i]);
 	}
 	//метод для помещения символа в очерредь 
 	void put(char ch) {
@@ -58,6 +77,38 @@ class QDemo {
        		System.out.println();
 		//попытка доступа к закрытым переменным класса Queue
 		//bigQ.q[3] = 343;
-		//bigQ.putloc = 3;		
-	}
+		//bigQ.putloc = 3;
+		//
+		Queue q1 = new Queue(10);
+		char[] chars = {'A', 'B', 'C'};
+		//
+		Queue q2 = new Queue(chars);
+		for(i = 0; i<10; i++)
+			q1.put((char) 'D'+i);
+		//создание очереди на базе другой очереди
+		Queue q3 = new Queue(q3); 
+
+		System.out.print("очередь q1: ");
+		for(i =0; i<10; i++) {
+			ch = q1.get();
+			System.out.print(ch);		
+		}
+		System.out.print("\n");
+
+		System.out.print("очередь q2: ");
+                for(i =0; i<3; i++) {
+                        ch = q2.get();
+                        System.out.print(ch);
+                }
+                System.out.print("\n");
+		
+
+		System.out.print("очередь q3: ");
+                for(i =0; i<10; i++) {
+                        ch = q3.get();
+                        System.out.print(ch);
+                }
+                System.out.print("\n");
+
+	}		
 }
