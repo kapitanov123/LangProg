fb12060099b157bf1d8bcfc6df69ad5ac10f25f8
kapitanov <kapitanov2002611@gmail.com>
2024-11-25 17:17:19 +0700

    алгоритм быстрой сортировки

---
+++ QSDemo.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+class Quicksort {
+	//вызов фактического методабыстрой сортировки
+	static void qsort(char[] items) {
+		qs(items, 0, items.length-1);
+	}
+	//рекурсивная версия быстрой сортирорвки
+	private static void qs(char[] items, int left, int right) {
+		int i, j;
+		char x, y;
+		i = left; 
+		j = right;
+		x = items[(left+right)/2];
+
+		do {
+			while((items[i] < x) && (i < right))
+				i++;
+			while((x < items[j]) && (j > left)) 
+				j--;
+			if(i <= j) {
+				y = items[i];
+				items[i] = items[j];
+				items[j] = y;
+				i++;
+				j--;
+			}	
+		} while (i <= j);
+		if (left < j) qs(items, left, j);
+		if (i < right) qs(items, i, right);
+	}
+}
+class QSDemo {
+	public static void main(String[] args) {
+		char[] a = {'d','a','f','e','z','k','w','f','c','m','w'};
+		int i;
+		System.out.print("исходный массив: ");
+		for(i=0; i<a.length; i++)
+			System.out.print(a[i]);
+		System.out.println();
+		//sortirovka
+		Quicksort.qsort(a);
+		System.out.print("отсортированный массив: ");
+		for(i=0; i<a.length; i++)
+                        System.out.print(a[i]);
+                System.out.println();
+
+		char[] b = new char[100000];
+		Random rand = new Random();
+		for (i=0; i<100000; i++) {
+			b[i] = (char) (rand.nextInt(75) + 48);
+       		}
+		Quicksort.qsort(b);
+                System.out.print("отсортерованый массив: ");
+                for(i=0; i<200; i++)
+                        System.out.print(b[i]);
+                System.out.println();
+		for(i=(b.length-1); i>(b.length-100); i--)
+                        System.out.print(b[i]);
+                System.out.println();
+	}
+}
