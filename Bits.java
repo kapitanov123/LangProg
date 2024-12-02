40c6e5de090ca06570d6a226f9761f7d689690c3
kapitanov <kapitanov2002611@gmail.com>
2024-12-02 15:26:26 +0700

    пример использования внутреннего класса

--- Bits.java
+++ Bits.java
@@ -1,28 +1,43 @@
 import java.util.Random;
-class ShowBits {
-	int numbits;
-	ShowBits(int n) {
-		numbits = n;
-	}
-	void show(long val) {
-		long mask = 1;
+class Bits {
+	public static void main(String[] args) {
+		class ShowBits {
+			int numbits;
+			ShowBits(int n) {
+			numbits = n;
+			}
+		void show(long val) {
+			long mask = 1;
 
 		//sdvig
-		mask <<= numbits-1;
+			mask <<= numbits-1;
 
-		int spacer = 0;
-		for(; mask !=0; mask >>>=1) {
-			if((val & mask) !=0);
-			else System.out.print("0");
-			spacer++;
-			if((spacer % 8) == 0) {
-				System.out.print(" ");
-				spacer = 0;
+			int spacer = 0;
+			for(; mask !=0; mask >>>=1) {
+				if((val & mask) !=0);
+				else System.out.print("0");
+				spacer++;
+				if((spacer % 8) == 0) {
+					System.out.print(" ");
+					spacer = 0;
+				}
 			}
+			System.out.println();
 		}
-		System.out.println();
+
+		}
+        	for(byte b = -128; b<127; b++) {
+                	ShowBits byteval = new ShowBits(8);
+                	System.out.print(b + "в двоичном виде");
+                	byteval.show(b);
+			if(b==127)
+				break;
+        	}	
+
 	}
+
 }
+/*
 class Bits {
 	public static void main(String[] args) {
 		//сброс 6 го бита - изменение регистра латиницы
@@ -156,3 +171,4 @@ class Bits {
 		}	
 	}	
 }
+*/
