1c334e48815fd29ce3a704a4c4cdf30274956f7b
kapitanov <kapitanov2002611@gmail.com>
2024-12-09 15:48:25 +0700

    добавили демонстрацию полиморфизма и класс цветной треугольник

--- Interihance.java
+++ Interihance.java
@@ -41,6 +41,10 @@ class TwoDShape {
 		return super.name;
 	}
 	*/
+	double area() {
+		System.out.println("метод должен быть переопределен!");
+		return 0.0;
+	}
 }
 class Triangle extends TwoDShape {
 	String style;
@@ -62,6 +66,17 @@ class Triangle extends TwoDShape {
 	}
 	*/
 }
+class ColorTriangle extends Triangle {
+	String color;
+	ColorTriangle(String c, String s, double w, double h) {
+		super(s, w, h); // vyzov konstruktora Triangle();
+		color = c;
+		name = "цветной треугольник";
+	}
+	void showColor() {
+		System.out.println("цвет: " + color);
+	}
+}
 class Rectangle extends TwoDShape {
 	Rectangle(String o, double w, double h){
 		super(w, h); //вызов конструктора супер класса
@@ -82,7 +97,7 @@ class Interihance {
 	public static void main(String[] args) {
 		TwoDShape fig1 = new TwoDShape(20.0, 30.0);
 		Triangle t1 = new Triangle("закрашенный", 4.5, 7.5);
-		Triangle t2 = new Triangle("контурный", 3.5, 8.0);
+		Triangle t2 = new ColorTriangle("синий", "контурный", 3.5, 8.0);
 		Rectangle r1 = new Rectangle("сплошная линия", 5.0, 5.0);
 		Rectangle r2 = new Rectangle("пунктирная линия", 5.0, 7.0);
 		System.out.println("информация об объeктах");
@@ -97,7 +112,26 @@ class Interihance {
 		System.out.println("r1 " + r1.name);
 		System.out.println("квадрат " + r1.isSquare());
 		System.out.println("площадь: " + r1.area());
+		//совместиомсть переменных родственных классов
+		//переменная супер класса может ссылаться на объект
+		System.out.println();
 
+		System.out.println("ширина двумерной фигуры fig1: " + fig1.getWidth());
+		//при указания на объект суперклассавыполняется метод суперкласса
+		fig1.area();
+		fig1 = t1;
+		System.out.println("ширина треугольника: " + fig1.getWidth());
+		//при указании на объект подкласса треугольник выполняется вычислениеплощади для треугольника
+		System.out.println("площадь треугольника: " + fig1.area());
+		fig1 = r1;
+		System.out.println("ширина прямоугольника: " + fig1.getWidth());
+		System.out.println("площадь прямоугольника: " + fig1.area());
+
+		fig1 = t2;
+		t2.showColor();
+		System.out.println("площадь цветного треугольника: " + fig1.area());
+		
+		
 		//проверка доступности переменных
 		/*
 		System.out.println(fig1.width);
